f27f341aabdb7a976c115f8a02fd0c4474fad39c
Elio Rosabal <elio_erp@hotmail.com>
2017-10-13 01:58:57 -0400

    pending sql database

--- GeekText/Data/ApplicationDbContext.cs
+++ GeekText/Data/ApplicationDbContext.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GeekText.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using GeekText.Models;
 
 namespace GeekText.Data
 {
@@ -15,12 +11,15 @@ namespace GeekText.Data
         {
         }
 
+        public DbSet<Book> Books { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder);
+            //base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+            builder.Entity<Book>().ToTable("Books");
         }
     }
 }
